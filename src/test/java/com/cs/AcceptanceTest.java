c75ca71880ce0df459784d561ff5345f73fd52ea
unclejamal <unclejamal@gmail.com>
2017-03-30 13:35:18 +0100

    Add missing bit of the requirement to the comment

--- src/test/java/com/cs/AcceptanceTest.java
+++ src/test/java/com/cs/AcceptanceTest.java
@@ -21,6 +21,13 @@ public class AcceptanceTest {
 //            5.5 kg for £306 // order a + order d
 //    1.5 kg for £307 // order c
 //    1.2 kg for £310 // order b
+
+//    The first thing to note here is that orders for the same price should be merged together
+//    (even when they are from different users).
+//    In this case it can be seen that order a) and d) were for the same amount (£306) and this is why only their sum (5.5 kg) is displayed (for £306)
+//    and not the individual orders (3.5 kg and 2.0 kg).The last thing to note is that for SELL orders the orders with lowest prices are displayed first.
+//    Opposite is true for the BUY orders.
+
     @Test
     public void requirement3_GetSummaryInformationOfLiveOrders() throws Exception {
         OrderService orderService = new OrderService();
